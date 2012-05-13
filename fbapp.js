5da7d5d4a8ef36cd1ec76fcad657c020861726c5
Elad Ben-Israel <elad.benisrael@gmail.com>
2012-05-13 04:18:51 +0300

    Update fbapp.js

--- fbapp.js
+++ fbapp.js
@@ -1,4 +1,4 @@
 exports.app_id = '305994549480898';
 exports.app_secret = 'ce989a0d98978dabe4955b5678bbc4ab';
 exports.scope = 'email';
-exports.callback_url = 'http://localhost:5000/facebook/auth';
+exports.callback_url = 'http://pbt.listzz.com/facebook/auth';
